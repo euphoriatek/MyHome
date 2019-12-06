d77163c0f31cbcd713e1092f13c9caabf7ff50da
euphoriatek <euphoriatek2010@gmail.com>
2019-12-06 19:58:48 +0530

    object page unstable

--- src/app/room-object/room-object.page.ts
+++ src/app/room-object/room-object.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,ViewChild } from '@angular/core';
+import {OwlCarousel} from 'ngx-owl-carousel';
 @Component({
   selector: 'app-room-object',
   templateUrl: './room-object.page.html',
   styleUrls: ['./room-object.page.scss'],
 })
 export class RoomObjectPage implements OnInit {
+  @ViewChild('owlElement',{'static':false}) owlElement: OwlCarousel
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  slides = [
+    {img: "./assets/images/living-room.jpg"},
+    {img: "./assets/images/living-room.jpg"},
+    {img: "./assets/images/living-room.jpg"},
+    {img: "./assets/images/living-room.jpg"},
+    {img: "./assets/images/living-room.jpg"},
+  ];
 }
